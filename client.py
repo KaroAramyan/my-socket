f207859a2f949c0422757abffb6ef477c696471a
KaroAramyan <144725187+KaroAramyan@users.noreply.github.com>
2024-05-16 09:42:29 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,84 @@
+import socket
+import sys
+from algorithms import rsa, rle
+import threading
+from ast import literal_eval as make_tuple
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QTextEdit, QPushButton, QLineEdit
+
+HEADER = 64
+PORT = 5050
+HOST = "localhost"
+ADDR = (HOST, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECT"
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+connected = True
+
+public_key_length = client.recv(HEADER).decode(FORMAT)
+if public_key_length:
+    public_key = client.recv(int(public_key_length)).decode(FORMAT)
+    public_key = make_tuple(public_key)
+
+first_msg_length = client.recv(HEADER).decode(FORMAT)
+if first_msg_length:
+    first_message = client.recv(int(first_msg_length)).decode(FORMAT)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Messaging App')
+        self.setGeometry(100, 100, 600, 400)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        grid_layout = QGridLayout()
+        central_widget.setLayout(grid_layout)
+
+        self.message_box = QTextEdit()
+        self.message_box.setReadOnly(True)
+        grid_layout.addWidget(self.message_box, 0, 0, 1, 2)
+        self.message_box.append(first_message)
+
+        self.input_box = QLineEdit()
+        grid_layout.addWidget(self.input_box, 1, 0)
+
+        self.send_button = QPushButton('Send')
+        self.send_button.clicked.connect(self.send_message)
+        grid_layout.addWidget(self.send_button, 1, 1)
+
+    def send_message(self):
+        global connected
+        message = self.input_box.text()
+        self.input_box.setText('')
+        if message == DISCONNECT_MESSAGE:
+            connected = False
+        ciphertext = rsa.encrypt(message, public_key)
+        ciphertext = rle.encode(ciphertext)
+        self.message_box.append("You: " + message)
+        message = ciphertext.encode(FORMAT)
+        send_length = str(len(message)).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+
+    def receive_message(self):
+        while connected:
+            msg_length = client.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = client.recv(msg_length).decode(FORMAT)
+                msg = rle.decode(msg)
+                plaintext = rsa.decrypt(msg, public_key)
+                self.message_box.append(plaintext)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    thread = threading.Thread(target=window.receive_message)
+    thread.start()
+    sys.exit(app.exec_())
---
+++ server.py
@@ -0,0 +1,93 @@
+import socket
+import threading
+from copy import copy
+
+from algorithms import rsa, rle
+
+HEADER = 64
+PORT = 5050
+HOST = "localhost"
+ADDR = (HOST, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECT"
+
+p = 61
+q = 53
+public_key, private_key = rsa.generate_key_pair(p, q)
+public_key = str(public_key)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+client_list = []
+client_number = 0
+
+
+def handle_client():
+    client_number, conn, addr = client_list[-1]
+    other_clients = client_list[:-1]
+
+    print("Connected:", addr)
+    first_message = 'You are client ' + str(client_number) + '. Successful connection. '
+    if len(other_clients) == 0:
+        first_message += " Waiting for other clients."
+
+    send_length = str(len(str(public_key))).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(public_key.encode(FORMAT))
+
+    send_length = str(len(first_message)).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(first_message.encode(FORMAT))
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            msg = rle.decode(msg)
+            plaintext = rsa.decrypt(msg, private_key)
+            print(msg, '/', plaintext)
+
+            if plaintext == DISCONNECT_MESSAGE:
+                connected = False
+                plaintext = "disconnected from chat."
+
+            plaintext = "Client " + str(client_number) + ": " + plaintext
+            send_message(plaintext, (client_number, conn, addr))
+
+    conn.close()
+    client_list.remove((client_number, conn, addr))
+    print("Disconnected:", addr)
+
+
+def send_message(message, sender_details):
+    ciphertext = rsa.encrypt(message, private_key)
+    ciphertext = rle.encode(ciphertext)
+    message = ciphertext.encode(FORMAT)
+
+    other_clients = copy(client_list)
+    other_clients.remove(sender_details)
+
+    send_length = str(len(message)).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+
+    for other_client in other_clients:
+        other_conn = other_client[1]
+        other_conn.send(send_length)
+        other_conn.send(message)
+
+
+if __name__ == "__main__":
+    print("Server is starting...")
+    server.listen()
+    print(f"Server is listening on host: {HOST}")
+    while True:
+        conn, addr = server.accept()
+        client_number += 1
+        client_list.append((client_number, conn, addr))
+        thread = threading.Thread(target=handle_client)
+        thread.start()
